9873a842815a7abfe7b0ec31b38b17be1b3f749c
cpraveen <cpraveen@gmail.com>
2011-01-31 16:00:59 +0000

    Switching to use primitive variables rather than conserved variables

--- src/material.cc
+++ src/material.cc
@@ -66,12 +66,32 @@ ConVar ConVar::operator* (const double scalar) const
    return result;
 }
 
+PrimVar PrimVar::operator* (const double scalar) const
+{
+   PrimVar result;
+
+   result.density  = density  * scalar;
+   result.velocity = velocity * scalar; 
+   result.pressure = pressure * scalar;
+
+   return result;
+}
+
+PrimVar& PrimVar::operator+= (const PrimVar& prim_var)
+{
+   density  += prim_var.density;
+   velocity += prim_var.velocity;
+   pressure += prim_var.pressure;
+
+   return *this;
+}
+
 // Set conserved variable to zero
-void ConVar::zero ()
+void PrimVar::zero ()
 {
    density  = 0.0;
-   momentum = 0.0;
-   energy   = 0.0;
+   velocity = 0.0;
+   pressure = 0.0;
 }
 
 double ConVar::pressure () const
@@ -138,8 +158,8 @@ Flux Flux::operator* (const double scalar)
 }
 
 // Numerical flux function
-void Material::num_flux (const ConVar& state_left,
-                         const ConVar& state_right,
+void Material::num_flux (const PrimVar& left,
+                         const PrimVar& right,
                          const Vector& normal,
                          Flux& flux)
 {
@@ -147,32 +167,29 @@ void Material::num_flux (const ConVar& state_left,
    double area = normal.norm();
    Vector unit_normal = normal / area;
 
-   PrimVar prim_left  = con2prim(state_left);
-   PrimVar prim_right = con2prim(state_right);
-
    // Enthalpy
-   double h_left  = GAMMA*prim_left.pressure/(prim_left.density*(GAMMA-1.0))
-      + 0.5 * prim_left.velocity.square();
-   double h_right = GAMMA*prim_right.pressure/(prim_right.density*(GAMMA-1.0))
-      + 0.5 * prim_right.velocity.square();
+   double h_left  = GAMMA*left.pressure/(left.density*(GAMMA-1.0))
+      + 0.5 * left.velocity.square();
+   double h_right = GAMMA*right.pressure/(right.density*(GAMMA-1.0))
+      + 0.5 * right.velocity.square();
 
-   double rho_left_sqrt = sqrt(prim_left.density);
-   double rho_right_sqrt = sqrt(prim_right.density);
+   double rho_left_sqrt = sqrt(left.density);
+   double rho_right_sqrt = sqrt(right.density);
    double fact_left = rho_left_sqrt / (rho_left_sqrt + rho_right_sqrt);
    double fact_right = 1.0 - fact_left;
 
    // Roe average state
    double density  = rho_left_sqrt * rho_right_sqrt;
-   Vector velocity = prim_left.velocity  * fact_left + 
-                     prim_right.velocity * fact_right;
+   Vector velocity = left.velocity  * fact_left + 
+                     right.velocity * fact_right;
    double h = h_left * fact_left + h_right * fact_right;
 
    double vel_normal = velocity * unit_normal;
    double c = sqrt( (GAMMA-1.0) * (h - 0.5*velocity.square()) );
 
-   double dp = prim_right.pressure - prim_left.pressure;
-   double vel_left_normal  = prim_left.velocity  * unit_normal;
-   double vel_right_normal = prim_right.velocity * unit_normal;
+   double dp = right.pressure - left.pressure;
+   double vel_left_normal  = left.velocity  * unit_normal;
+   double vel_right_normal = right.velocity * unit_normal;
    double dV = vel_right_normal - vel_left_normal;
 
    if(vel_normal > 0.0)
@@ -182,9 +199,9 @@ void Material::num_flux (const ConVar& state_left,
       double factor = min(lambda, 0.0) * coeff;
 
       // Left flux
-      flux.mass_flux = state_left.density * vel_left_normal;
-      flux.momentum_flux = unit_normal * prim_left.pressure +
-                           state_left.momentum * vel_left_normal;
+      flux.mass_flux = left.density * vel_left_normal;
+      flux.momentum_flux = unit_normal * left.pressure +
+                           left.velocity * left.density * vel_left_normal;
       flux.energy_flux = h_left * flux.mass_flux;
 
       // Upwind term
@@ -199,9 +216,9 @@ void Material::num_flux (const ConVar& state_left,
       double factor = max(lambda, 0.0) * coeff;
 
       // Right flux
-      flux.mass_flux = state_right.density * vel_right_normal;
-      flux.momentum_flux = unit_normal * prim_right.pressure +
-                           state_right.momentum * vel_right_normal;
+      flux.mass_flux = right.density * vel_right_normal;
+      flux.momentum_flux = unit_normal * right.pressure +
+                           right.velocity * right.density * vel_right_normal;
       flux.energy_flux = h_right * flux.mass_flux;
 
       // Upwind term
@@ -214,15 +231,13 @@ void Material::num_flux (const ConVar& state_left,
 }
 
 // Flux on slip walls
-Flux Material::slip_flux (const ConVar& state,
+Flux Material::slip_flux (const PrimVar& state,
                           const Vector& normal)
 {
-   double pressure = state.pressure ();
-
    Flux flux;
 
    flux.mass_flux     = 0.0;
-   flux.momentum_flux = normal * pressure;
+   flux.momentum_flux = normal * state.pressure;
    flux.energy_flux   = 0.0;
 
    return flux;
--- src/parameter.h
+++ src/parameter.h
@@ -1,6 +1,7 @@
 #ifndef __PARAMETER_H__
 #define __PARAMETER_H__
 
+#include <vector>
 #include <string>
 #include <map>
 #include "vec.h"
@@ -19,12 +20,14 @@ class Parameter
       double min_residue;
       double mach_inf;
       Vector velocity_inf;
-      ConVar con_inf;
+      PrimVar prim_inf;
+      ConVar  con_inf;
 
       std::string grid_file;
       GridType    grid_type;
 
-      std::map<int,BCType> bc;
+      std::map<int,BCType> bc_type;
+      std::map< int,std::vector<double> > bc_state;
 
       void read ();
 };
