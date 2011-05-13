44fa5d24e6a2a5326abc2b456905010e3d33ef20
cpraveen <cpraveen@gmail.com>
2011-05-13 06:35:15 +0000

    Modified cp plotting so that theta=0 is front stagnation point; theta=pi is rear stagnation point

--- examples/subsonic_sphere/cpplot.m
+++ examples/subsonic_sphere/cpplot.m
@@ -9,11 +9,12 @@ for j=1:n
    y = data(j,2);
    Cp(j)= data(j,4);
 
-   theta(j) = atan2(x,y);
-   Cpexact(j) = 1 - (9/4)*cos(theta(j))^2;
+   theta(j) = atan2(y,x);
+   Cpexact(j) = 1 - (9/4)*sin(theta(j))^2;
 end
 
 theta=abs(theta);
+theta=pi - theta;
 plot(theta,Cpexact,'-',theta,-data(:,4),'LineWidth',2)
 legend('Exact','flo3d')
 ylabel('C_p')
